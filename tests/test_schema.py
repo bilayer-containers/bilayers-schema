f31ead062e536f613146f13bfb4bea0734c2ea16
Rajavi Shah <shahraja@broadinstitute.org>
2026-07-13 16:10:29 -0400

    [restructure p4] ruff preview on; [del] extend-select is redundant on select

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -2,33 +2,40 @@ def test_schema_loads():
     from bilayers_schema import schema
     assert isinstance(schema, dict)
 
+
 def test_print_schema_yaml(capsys):
     from bilayers_schema import print_schema
     print_schema(as_yaml=True)
     assert "TypeInput" in capsys.readouterr().out
+
 
 def test_print_schema_pprint(capsys):
     from bilayers_schema import print_schema
     print_schema(as_yaml=False)
     assert "TypeInput" in capsys.readouterr().out
 
+
 def test_display_only_has_no_interactive(schema):
     slots = schema["classes"]["TypeDisplayOnly"].get("slots", [])
     assert "interactive" not in slots
 
+
 def test_parameter_has_interactive(schema):
     slots = schema["classes"]["TypeParameter"].get("slots", [])
     assert "interactive" in slots
+
 
 def test_required_top_level_slots(schema):
     assert schema["slots"]["inputs"]["required"] is True
     assert schema["slots"]["outputs"]["required"] is True
     assert schema["slots"]["exec_function"]["required"] is True
 
+
 def test_type_enum_has_expected_values(schema):
     values = schema["enums"]["TypeEnum"]["permissible_values"]
     for expected in ["image", "measurement", "checkbox", "dropdown", "radio", "textbox"]:
         assert expected in values
+
 
 def test_config_has_required_top_level_keys(classical_segmentation_config):
     config = classical_segmentation_config
@@ -40,6 +47,7 @@ def test_config_has_required_top_level_keys(classical_segmentation_config):
     assert "citations" in config
     assert "algorithm_folder_name" in config
 
+
 def test_input_image_type_has_image_fields(classical_segmentation_config):
     inputs = classical_segmentation_config["inputs"]
     image_input = next(i for i in inputs if i["type"] == "image")
@@ -48,6 +56,7 @@ def test_input_image_type_has_image_fields(classical_segmentation_config):
     assert "timepoints" in image_input
     assert "tiled" in image_input
     assert "pyramidal" in image_input
+
 
 def test_radio_parameter_has_options(classical_segmentation_config):
     params = classical_segmentation_config["parameters"]
